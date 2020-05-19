4ba7b44d27e5df91f7c16994f8c762a6ffdedd15
= <=>
2020-05-19 23:11:53 +0300

    added create database transaction

--- bd_course/admin_tools/admin_tool.py
+++ bd_course/admin_tools/admin_tool.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta
+from connect.connect_manager import ConnectManager
+from datetime import datetime
 from query_tool.abstract_query_tool import Commands
 
 
@@ -11,14 +12,39 @@ class AdminTool(Commands):
 
 
 class AdminPostgreSQL(AdminTool):
+    def create_database(self):
+        side_connect = ConnectManager('connection_admin.ini')
+        query = '''DO
+                            $do$
+                            DECLARE
+                              _db TEXT := %s;
+                              _user TEXT := %s;
+                              _password TEXT := %s;
+                            BEGIN
+                              CREATE EXTENSION IF NOT EXISTS dblink; -- enable extension 
+                              IF EXISTS (SELECT 1 FROM pg_database WHERE datname = _db) THEN
+                                RAISE NOTICE 'Database already exists';
+                              ELSE
+                                PERFORM dblink_connect(%s || _user || ' password=' || _password || ' dbname=' || current_database());
+                                PERFORM dblink_exec('CREATE DATABASE ' || _db);
+                              END IF;
+                            END
+                            $do$'''
+        host_server = 'port=' + self.connectmanager.connect_info['port'] + ' host=' + self.connectmanager.connect_info['host'] + ' user='
+        side_connect.cursor.execute(query, (self.connectmanager.connect_info['dbname'], self.connectmanager.connect_info['user'],
+                                            self.connectmanager.connect_info['password'], host_server))
+        side_connect.database.commit()
+        side_connect.__del__()
+        self.create_tables()
+
     def create_tables(self):
-        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS people_id''')
+        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS people_id;''')
         self.connectmanager.cursor.execute('''CREATE TABLE people_id
                         (id_people INT PRIMARY KEY NOT NULL,
                         name TEXT NOT NULL,
                         face BYTEA NOT NULL);''')
 
-        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS events''')
+        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS events;''')
         self.connectmanager.cursor.execute('''CREATE TABLE events
                         (id_event INT PRIMARY KEY NOT NULL,
                         name TEXT NOT NULL,
@@ -26,13 +52,13 @@ class AdminPostgreSQL(AdminTool):
                         current_detection TIMESTAMP NOT NULL,
                         location TEXT NOT NULL);''')
 
-        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS recognitions''')
+        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS recognitions;''')
         self.connectmanager.cursor.execute('''CREATE TABLE recognitions
                         (id_recognition INT PRIMARY KEY NOT NULL,
                         name TEXT NOT NULL,
                         transaction_time TIMESTAMP NOT NULL);''')
 
-        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS logs''')
+        self.connectmanager.cursor.execute('''DROP TABLE IF EXISTS logs;''')
         self.connectmanager.cursor.execute('''CREATE TABLE logs
                                 (name TEXT NOT NULL,
                                 description TEXT NOT NULL,
@@ -42,15 +68,18 @@ class AdminPostgreSQL(AdminTool):
 
     def push_log(self, table_name, exception_name):
         self.connectmanager.cursor.execute('''INSERT INTO logs (name, description, transaction_time) 
-                                              VALUES (%s, %s, %s)''',
+                                              VALUES (%s, %s, %s);''',
                                            (table_name, exception_name, datetime.now()))
         self.connectmanager.database.commit()
         self.currentid_log += 1
 
 
 class AdminInfluxDB(AdminTool):
+    def create_database(self):
+        pass
+
     def create_tables(self):
         pass
 
-    def push_log(self):
+    def push_log(self, table_name, exception_name):
         pass
