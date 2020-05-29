b7a7599abf2c75eb11fbc92610168b0e339a7477
= <=>
2020-05-29 21:02:46 +0300

    added log feature

--- bd_course/file_checker.py
+++ bd_course/file_checker.py
@@ -1,3 +1,5 @@
+from logger import MyLogger
+
 import os
 
 
@@ -5,30 +7,34 @@ class FileChecker:
     @staticmethod
     def check_files():
         path = os.getcwd()
-        result = True
+        result = 0
 
         if not os.path.exists(path + '\\configs\\connection.ini'):
-            print("Cannot find connection.ini file")
-            result = False
+            MyLogger.warning("Cannot find connection.ini file")
+            result = -1
+
+        if not os.path.exists(path + '\\configs\\connection_admin.ini'):
+            MyLogger.warning("Cannot find connection_admin.ini file")
+            result = -2
 
         if not os.path.exists(path + '\\models\\mdl.caffemodel'):
-            print("Cannot find mdl.caffemodel file")
-            result = False
+            MyLogger.warning("Cannot find mdl.caffemodel file")
+            result = -3
 
         if not os.path.exists(path + '\\models\\prt.txt'):
-            print("Cannot find prt.txt file")
-            result = False
+            MyLogger.warning("Cannot find prt.txt file")
+            result = -4
 
         if not os.path.exists(path + '\\models\\emt_mdl.hdf5'):
-            print("Cannot find emt_mdl.hdf5 file")
-            result = False
+            MyLogger.warning("Cannot find emt_mdl.hdf5 file")
+            result = -5
 
         if not os.path.exists(path + '\\configs\\camera.ini'):
-            print("Cannot find camera.ini file")
-            result = False
+            MyLogger.warning("Cannot find camera.ini file. You need to launch camera_calibrator.py. Find chess pattern picture and print it")
+            result = -6
 
         if not os.path.exists(path + '\\configs\\coordinates_setup.ini'):
-            print("Cannot find coordinates_setup.ini file")
-            result = False
+            MyLogger.warning("Cannot find coordinates_setup.ini file")
+            result = -7
 
         return result
