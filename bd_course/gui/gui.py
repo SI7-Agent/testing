803ddf41e0ff6115595d6fc1f8e4091356ec24f9
= <=>
2020-05-29 21:03:21 +0300

    added some restrictions

--- bd_course/gui/gui.py
+++ bd_course/gui/gui.py
@@ -8,26 +8,28 @@ class OpencvGui(gui):
         self.camera = cv2.VideoCapture(0)
 
     def make_ui(self, handler):
-        while True:
+        while self.camera.isOpened():
             ret, frame = self.camera.read()
-            objects = handler.find_objects(frame)
-            persons = handler.find_persons(frame)
 
-            for i in objects:
-                cv2.rectangle(frame, (i[1], i[2]), (i[3], i[4]), i[-1], 2)
-                cv2.putText(frame, i[0], (i[1], i[-2]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, i[-1], 2)
+            if ret:
+                objects = handler.find_objects(frame)
+                persons = handler.find_persons(frame)
 
-            #           =======================================================
+                for i in objects:
+                    cv2.rectangle(frame, (i[1], i[2]), (i[3], i[4]), i[-1], 2)
+                    cv2.putText(frame, i[0], (i[1], i[-2]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, i[-1], 2)
 
-            for i in persons:
-                cv2.rectangle(frame, (i[0], i[1]), (i[2], i[3]), i[-1], 2)
-                cv2.putText(frame, i[-2][0], (i[0] + 6, i[3] - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
-                cv2.putText(frame, i[-2][1], (i[0] + 6, i[1] - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                #           =======================================================
 
-            cv2.imshow('Video', frame)
+                for i in persons:
+                    cv2.rectangle(frame, (i[0], i[1]), (i[2], i[3]), i[-1], 2)
+                    cv2.putText(frame, i[-2][0], (i[0] + 6, i[3] - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                    cv2.putText(frame, i[-2][1], (i[0] + 6, i[1] - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.imshow('Video', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
                 break
-
         self.camera.release()
         cv2.destroyAllWindows()
