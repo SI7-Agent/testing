82bbde0ea3e93eec08981a8c402feb53d5e0b55c
= <=>
2020-05-29 21:00:51 +0300

    refactor choose method

--- bd_course/concrete_base_helper.py
+++ bd_course/concrete_base_helper.py
@@ -1,22 +1,11 @@
-from query_tool.insert_tool import InsertPostgreSQL
-from query_tool.select_tool import SelectPostgreSQL
-from query_tool.update_tool import UpdatePostgreSQL
-
-from query_tool.insert_tool import InsertInfluxDB
-from query_tool.select_tool import SelectInfluxDB
-from query_tool.update_tool import UpdateInfluxDB
+from query_tool.postgre_tool import PostgreSQLTool
+from query_tool.influx_tool import InfluxDBTool
 
 
 class BaseChooser:
     @staticmethod
     def choose(connect_manager):
         if connect_manager.database_type == 'postgresql':
-            insert_tool = InsertPostgreSQL(connect_manager)
-            select_tool = SelectPostgreSQL(connect_manager)
-            update_tool = UpdatePostgreSQL(connect_manager)
-            return (insert_tool, select_tool, update_tool)
+            return PostgreSQLTool(connect_manager)
         elif connect_manager.database_type == 'influxdb':
-            insert_tool = InsertInfluxDB(connect_manager)
-            select_tool = SelectInfluxDB(connect_manager)
-            update_tool = UpdateInfluxDB(connect_manager)
-            return (insert_tool, select_tool, update_tool)
+            return InfluxDBTool
