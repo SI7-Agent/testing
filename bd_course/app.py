1def03f19a05d94b744b7b585305bd8ca32f02b7
= <=>
2020-05-29 20:59:05 +0300

    fixed database creation

--- bd_course/app.py
+++ bd_course/app.py
@@ -3,21 +3,31 @@ from concrete_base_helper import BaseChooser
 from connect.connect_manager import ConnectManager
 from file_checker import FileChecker
 from gui.gui import OpencvGui
-from robot import Robot
+from logger import MyLogger
+from modules.concrete_system import MySystem
 
 
 class MyApplication:
     @staticmethod
     def start():
-        if FileChecker.check_files():
+        if (error := FileChecker.check_files()) > -1:
+            admin_connect = ConnectManager("connection_admin.ini")
+            AdminTool(admin_connect).admin_tool().create_database()
+
             my_connect = ConnectManager()
             my_base_commands = BaseChooser.choose(my_connect)
             try:
-                AdminTool(my_connect).admin_tool().create_database()
-                system = Robot(my_base_commands)
+                AdminTool(my_connect).admin_tool().create_tables()
+                AdminTool(my_connect).admin_tool().create_side_funcs()
+                system = MySystem(my_base_commands)
                 interface = OpencvGui()
                 interface.make_ui(system)
             except AttributeError:
-                print("Unable to create connection/database\n")
+                MyLogger.critical("Unable to create connection/database")
+            finally:
+                MyLogger.info("App is over")
+                exit(error)
         else:
-            print("Unable to find configuration files\n")
+            MyLogger.warning("Unable to find configuration files")
+            MyLogger.info("App is over")
+            exit(error)
