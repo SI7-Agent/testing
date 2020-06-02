ceb9903dc3697d06eeb3f47d02681307458f6b44
= <=>
2020-06-02 23:19:40 +0300

    features of srings upd

--- bd_course/string_methods.py
+++ bd_course/string_methods.py
@@ -6,3 +6,11 @@ class StringsMethods:
     @staticmethod
     def cut_to(string, suffix='\0'):
         return string[:string.find(suffix)] if suffix != '\0' else string[:]
+
+    @staticmethod
+    def user_by_url(album_url):
+        return album_url.split('/')[-1].split('_')[0].replace('album', '')
+
+    @staticmethod
+    def album_by_url(album_url):
+        return album_url.split('/')[-1].split('_')[1]
