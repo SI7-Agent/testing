879a57d4c40f768ef89cd68c1d56e3994d8b6e76
Kirill Rudnev <kirilvasya@gmail.com>
2020-11-15 16:43:41 +0300

    server name = app name

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,7 +8,7 @@ worker = MyWebApplication().start()
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
+    app = connexion.App('Object detection', specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Object detection'})
     CORS(app.app)
