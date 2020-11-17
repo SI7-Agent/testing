959f231c3d526b19853f251ed35d1b9a07941412
Kirill Rudnev <kirilvasya@gmail.com>
2020-11-17 20:05:18 +0300

    added support of launch on a custom port

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,5 +1,6 @@
 import connexion
 import encoder
+import sys
 from flask_cors import CORS
 from app import MyWebApplication
 from flask_cors import CORS
@@ -8,11 +9,22 @@ worker = MyWebApplication().start()
 
 
 def main():
+    try:
+        args = sys.argv
+    except AttributeError:
+        sys.argv = pymol_argv
+        args = sys.argv
+
+    try:
+        port = int(args[1]) if 0 <= int(args[1]) <= 65535 else 3333
+    except:
+        port = 3333
+
     app = connexion.App('Object detection', specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Object detection'})
     CORS(app.app)
-    app.run(port=3333)
+    app.run(port=port)
 
 
 if __name__ == '__main__':
