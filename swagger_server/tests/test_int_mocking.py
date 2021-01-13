364d834746aa9330c6fca179111f016295f91c36
Kirill Rudnev <kirilvasya#gmail.com>
2021-01-13 16:22:02 +0300

    upd prototype

--- swagger_server/tests/test_int_mocking.py
+++ swagger_server/tests/test_int_mocking.py
@@ -1,4 +1,16 @@
+import sqlite3
 import pytest
+
+
+
+@pytest.fixture()
+def database_env():
+    connection = sqlite3.connect(":memory:")
+    cursor = connection.cursor()
+    sql_file = open("setup.sql")
+    sql_as_string = sql_file.read()
+    cursor.executescript(sql_as_string)
+    return cursor
 
 
 def test_int_human_module_and_gender_module_mock():
